2661fd2aa339a93a44ae451a43434eb4150fd0b0
Charles Erickson <cse621@gmail.com>
2019-06-21 11:57:52 -0500

    added ingrediant model

---
+++ recipeBox/recipeBox/Models/Ingrediants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace recipeBox.Models
+{
+    public class Ingrediants
+    {
+        [Key]
+        public int IngrediantId { get; set; }
+        public string Name { get; set; }
+        
+        [ForeignKey("Recipe")]
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
